7d066e8b1b64a91d1152508f8098c58bc471ef41
haq2005 <91720352+haq2005@users.noreply.github.com>
2023-11-09 15:15:55 +0545

    newsletter
    newsletter commit by haq at nov9

--- api/index.js
+++ api/index.js
@@ -3,13 +3,14 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const blog = require("./routes/blogs");
+var newsLetter = require('./routes/newsLetter')
 //basic setup
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 app.use("/blog", blog);
-
+app.use('/newsLetter',newsLetter)
 app.get("/", (req, res) => res.send("OK"));
 app.listen(8001, (err) => {  
   if (err) {
